ffa62ecca65d5b4feab25e6994e0ea6a9cacd903
John Miller <johnmill@microsoft.com>
2018-06-09 09:44:42 -0400

    Initial Azure function. It doesn't seem to run

--- src/CommitArt.Function/Function1.cs
+++ src/CommitArt.Function/Function1.cs
@@ -1,4 +1,5 @@
 using System;
+using CommitArt.Core;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 
@@ -9,7 +10,30 @@ namespace CommitArt
         [FunctionName("Function1")]
         public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, TraceWriter log)
         {
-            log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+            try
+            {
+                var updater = AppSettingsInitGitUpdater();
+                log.Info($"{DateTime.Now}: Getting git repo");
+                updater.CloneRepo();
+                updater.UpdateLocalDataFile();
+                log.Info($"{DateTime.Now}: Data File updated");
+                updater.PushChangesToGithub();
+                log.Info($"{DateTime.Now}: GitHub updated");
+            }
+            catch (Exception e)
+            {
+                log.Error($"{DateTime.Now}: {e.Message} - {e.StackTrace}");
+            }
+        }
+
+        private static GitUpdater AppSettingsInitGitUpdater()
+        {
+            string remoteUrl = Environment.GetEnvironmentVariable("GitRemoteUrl");
+            string username = Environment.GetEnvironmentVariable("GitUsername");
+            string password = Environment.GetEnvironmentVariable("GitPassword");
+            string email = Environment.GetEnvironmentVariable("GitEmail");
+
+            return new GitUpdater(remoteUrl, username, password, email);
         }
     }
 }
